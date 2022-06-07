b6b343ec63204c24e5f2ca3768903e6b81d34b9b
Micah <micahburnside@protonamil.com>
2022-06-07 14:39:33 -0400

    imports Pencil Kit. Embeds Main.storyboard into a Navigation Controller. Adds two BarButtonItems to the navBar. Connects the two BarButtonItems to ViewController.swift. Adds canvasView as PKCanvasView. . Conforms to PKCanvasViewDelegate. Inherits PKToolObserver. Sets canvasView delegate to ViewController. Sets canvasView width and height. Adds toolPicker to parent view (window)

--- PenckilKitProject/ViewController.swift
+++ PenckilKitProject/ViewController.swift
@@ -8,13 +8,40 @@
 import UIKit
 import PencilKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
+
+    @IBOutlet weak var canvasView: PKCanvasView!
+    @IBOutlet weak var takeScreenshotBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var pencilBarButtonItem: UIBarButtonItem!
+    
+    let canvasWidth: CGFloat = 768
+    let canvasOverscrollingHeight: CGFloat = 500
+    var drawing = PKDrawing()
+    let toolPicker = PKToolPicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        canvasView.delegate = self
+        canvasView.drawing = drawing
+        
+        canvasView.alwaysBounceVertical = true
+        canvasView.drawingPolicy = .anyInput
+        
+        if let window = parent?.view.window {
+           toolPicker.setVisible(true, forFirstResponder: canvasView)
+           toolPicker.addObserver(canvasView)
+            canvasView.becomeFirstResponder()
+        }
+        
     }
-
-
+    
+    @IBAction func saveImagePressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func selectStylusType(_ sender: UIBarButtonItem) {
+        
+    }
+    
 }
 
